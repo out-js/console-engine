e5bd4066d4c1dc036db702a5c4a371268cb98ccf
Patrick <paradaysazsiti@gmail.com>
2021-08-18 19:46:53 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple ACCII engine
+
+
---
+++ engine.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+extern unsigned char width, height ; 
+
+
+extern void generateArea ( )
+{
+    extern char area [ width ];
+    for ( unsigned char i = width ; i > 0 ; i-- )
+    {
+        area[i] = [height]
+    }
+} 
+
+extern void printArea ( ) 
+{
+    for ( unsigned char x = width ; x > 0 ; x-- )
+    {
+        for ( unsigned char y = height ; y > 0 ; y-- )
+        {
+            
+        }
+    }
+    return;
+}
